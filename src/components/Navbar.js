f5e1e329a8c10e3d4d280e45dff710e7cbf7d7cf
Ichsan <ichsan.bukit2014@gmail.com>
2022-08-20 20:46:14 +0700

    React test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,7 +43,7 @@ function Navbar() {
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/service" className="nav-links" onClick={closeMobileMenu}>
+              <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
                 Services
               </Link>
             </li>
